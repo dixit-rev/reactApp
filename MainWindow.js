963577822fec41ea7fd85f5c8276cd39972000b3
dexter-code <dixitrajpura1@gmail.com>
2017-11-13 17:24:38 +0530

    add new proj

---
+++ MainWindow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MainWindow extends React.Component {
+    render() {
+        return (
+            <div className=" row">
+                <div className="col-sm-12">
+                    main row starts here
+                </div>
+
+            </div>
+        );
+    }
+}
+export default MainWindow;
---
+++ main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainWindow from './MainWindow.js';
+
+ReactDOM.render(<MainWindow />, document.getElementById('app'));
